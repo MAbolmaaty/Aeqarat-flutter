56aebddef70cc3cb29c5ec401bef55ab5e9f976a
ahmedgado95 <ahmedgado2001@gmail.com>
2021-01-07 10:59:53 +0200

    update structure Commite

--- lib/screens/welcomePage.dart
+++ lib/screens/welcomePage.dart
@@ -12,7 +12,7 @@ class _WelcomePageState extends State<WelcomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Welcome"),
+        title: Text("Welcomee"),
       ),
     );
   }
